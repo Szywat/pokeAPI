fa66687c4f2cf36901f69b89ee88411674e07008
Szymon Wątorowski <szywat04@gmail.com>
2024-12-17 10:26:17 +0100

    filter test

---
+++ pokeapi/app/components/Filter.js
@@ -0,0 +1,12 @@
+export default function Filtering() {
+    return (
+        <div>
+            <select name="numbers">
+                <option value="1"></option>
+                <option value="2">1</option>
+                <option value="3">2</option>
+            </select>
+            <button>Filter!</button>
+        </div>
+    )
+}
